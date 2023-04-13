a98b16763639bacaff4bb416b41655ece00d9a1d
ToF- <cthibauttof@gmail.com>
2023-04-13 09:54:17 +0200

    done

--- src/Change.hs
+++ src/Change.hs
@@ -5,6 +5,4 @@ change :: Integer -> [Integer] -> Integer
 change n _ | n < 0 = 0
 change 0 _ = 1
 change _ [] = 0
-change n [c] = change (n-c) [c]
-change n [c0,c1] = change (n-c0) [c0,c1] + change (n-c1) [c1]
-change _ _ = 10
+change n (c0:cs) = change (n-c0) (c0:cs) + change n cs
